252e8853091ceaefdadc04fd75e3a10de269e882
Felipe Marini <felipemarini71188@gmail.com>
2020-12-01 20:37:18 -0300

    Algorithm finished with few things left to do

--- Program.cs
+++ Program.cs
@@ -6,136 +6,122 @@ namespace Product_Registration_Algorithm
     {
         static void Main(string[] args)
         {
-
+            int choice;
             int counter = 0;
-            int choice = 0;
             string answer;
-            string answer2 = "";
-            
+            string[] names = new string [2];                        
+            float[] prices = new float [2];
+            bool [] sales = new bool [2] ;
 
-            string[] names = new string [3];
-            float[] prices = new float [3];
-            bool[] sales = new bool [3];
-             
+            Console.WriteLine("-------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Welcome to our Product Registration System");
+            Console.ResetColor();
+            Console.WriteLine("-------------------------");
 
-
-        do {
-        
-        Console.WriteLine();    
-        Console.WriteLine("Welcome to the Product Registration System");
-        Console.WriteLine();
-
-        Console.WriteLine("Main Menu");
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine("[1] Product Registration");
-        Console.WriteLine();
-        Console.WriteLine("[2] List of Registered Products");
-        Console.WriteLine();
-        Console.WriteLine("[3] Exit Main Menu");
-        Console.WriteLine();
-        Console.Write("Choose your option: ");
-
-        choice = int.Parse(Console.ReadLine());
-
-        
-         
-        
-        switch (choice) {
-        
-        
-        case (1): 
-
-        if (counter < 3) {
-            
             do
-            {
-                    Console.WriteLine();
-                    Console.WriteLine($"Please type in Product {counter + 1} name:\n ");
-                    names[counter] = Console.ReadLine();
-                    Console.WriteLine();
-                    
-                    Console.WriteLine();
-                    Console.Write($"Please type in Product {counter + 1} price:  $ ");
-                    prices[counter] = float.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine();
+            {   
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+                Console.WriteLine("Menu");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("[1] Register product");
+                Console.WriteLine();
+                Console.WriteLine("[2] List registered products");
+                Console.WriteLine();
+                Console.WriteLine("[3] Exit menu");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Please choose one of the following options:\n");
+                Console.WriteLine();
+                choice = int.Parse(Console.ReadLine());
+                Console.ResetColor();
 
-                    do {
-                    
-                    Console.WriteLine($"Is Product {names[counter]} on sale?  [yes = y] or [no = n] \n ");
-                    answer = Console.ReadLine().ToLower();
-                    
-                    if (answer == "y")  {answer2 = "on sale";}
-
-                    else if (answer == "n") {answer2 = "not on sale";}
-
-                    else if (answer != "y" && answer != "n") {
-
+            switch (choice)
+                {   
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
                         Console.WriteLine();
-                        Console.WriteLine("Invalid option");
+                        Console.WriteLine("Product Registration");
+                        Console.WriteLine();
+                        do
+                        {
+                            if(counter < 2 ){
+                            Console.WriteLine($"Please type the name of product {counter+1}:\n");
+                            names[counter] = Console.ReadLine();
+                            Console.WriteLine();
+                            
+                            Console.WriteLine();
+                            Console.Write($"Please type price of product {counter+1}: $ ");
+                            prices[counter] = float.Parse(Console.ReadLine());
+                            Console.WriteLine();
+
+                            Console.WriteLine($"Is product {names[counter]} on sale? [true] / [false] ");
+                            Console.WriteLine();
+                            sales[counter] = bool.Parse(Console.ReadLine());
+                            Console.WriteLine();
+
+                            counter++;
+
+                            }
+                            
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Product Registration Limit Exceeded");
+                                Console.WriteLine();
+                                break;
+                            }
+                            
+                            Console.WriteLine();
+                            Console.WriteLine("Would you like to register another product? [yes = y]  [no = n]\n");
+                            Console.WriteLine();
+                            answer = Console.ReadLine().ToUpper();
+                            Console.ResetColor();
+                        
+                        }  while (answer == "Y");
+
+                        break;
+
+                    case 2:
+                        
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.WriteLine("List of registered products:");
                         Console.WriteLine();
                         
-                       }
-
+                        for (var i = 0; i < counter; i++)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"product: {names[i]}");
+                            Console.WriteLine();
+                            Console.WriteLine($"price:$ {prices[i].ToString("N2")}");
+                            Console.WriteLine();
+                            Console.WriteLine($"On Sale? :{sales[i]}");
+                        }
+                        Console.ResetColor();
+                        break;
                     
-                    }   while (answer != "y" && answer != "n");
-
-            
-                    counter ++;
-
+                    case 3:
+                        
+                        Console.WriteLine();
+                        Console.WriteLine("Thank you for using our System");
+                        break;
+                    
+                    default:
+                        
+                        Console.Clear();
+                        break;
                 
-            } while (counter < 3);
-                
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Limit for Product Registration reached");
-                Console.WriteLine();
-               
-               }
 
-               break;
-
-
-
-        case (2): 
-
-                Console.WriteLine();
-                Console.WriteLine("Here is your list of registered Products:");
-                Console.WriteLine();
-                Console.WriteLine();
-
-                for (var i = 0; i < names.Length; i++)
-                {
-                
-                Console.WriteLine();    
-Console.WriteLine($"name: {names[i]} | price: ${prices[i].ToString("N2")} | current status: {answer2}");
-                
-                }
-
-                Console.WriteLine();
-
-                break;
-
-        case (3):
-
-                Console.WriteLine();
-                Console.WriteLine("Thanks for using our Product System");
-
-                break;
-
-        
-        default: Console.Clear();
-                 
-                 break;
-        
-        }
-
-    
-    
-        } while (choice != 3);
-        
-        
+            } 
+   
+            } while (choice != 3);
         }
     }
 }
